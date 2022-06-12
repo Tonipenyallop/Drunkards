254126ceabd1f579fabadcff201e6031cc8bee50
Tonipenyallop <kimtesu521@gmail.com>
2022-06-12 12:30:59 +0900

    Refactor

--- src/webserver/server.ts
+++ src/webserver/server.ts
@@ -1,17 +1,14 @@
 import  { Application, Request, Response, NextFunction } from "express";
-
 import path from "path";
 import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
 import { ProtoGrpcType } from "../proto/index";
-import { Http2ServerRequest } from "http2";
-// import express from "express";
+
 const express = require("express")
 const cors = require("cors")
-
 const app = express();
-const port = 8080; // default port to listen
-
+const expressPORT = 8080; // default expressPORT to listen
+const PROTO_PATH = "../proto/index.proto";
 
 app.use(cors());
 app.use(
@@ -21,9 +18,6 @@ app.use(
   );
 app.use(express.json());
 
-
-const PROTO_PATH = "../proto/index.proto";
-
 const packageDefinition = protoLoader.loadSync(
   path.resolve(__dirname, PROTO_PATH)
 );
@@ -32,18 +26,14 @@ const grpcObj = grpc.loadPackageDefinition(
   packageDefinition
 ) as unknown as ProtoGrpcType;
 
-const PORT = 8882;
-// tryna make it same as envoy port
+const GRPCPORT = 8882;
 
 const client = new grpcObj.index.User(
-  `0.0.0.0:${PORT}`,
+  `0.0.0.0:${GRPCPORT}`,
   grpc.credentials.createInsecure()
 );
 
 
-
-
-// define a route handler for the default home page
 app.post("/login", ( req: Request, res: Response ) => {
 
     client.Login(
@@ -64,6 +54,6 @@ app.post("/login", ( req: Request, res: Response ) => {
 } );
 
 // start the Express server
-app.listen( port, () => {
-    console.log(`server started at http://localhost:${ port }` );
+app.listen( expressPORT, () => {
+    console.log(`server started at http://localhost:${ expressPORT }` );
 } );
