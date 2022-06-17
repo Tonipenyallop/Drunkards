085f73bc7be87e9ba2119bf86006b89f5c46f959
Tonipenyallop <kimtesu521@gmail.com>
2022-06-17 14:26:12 +0900

    Remove: inserting users

--- seeds/users.ts
+++ seeds/users.ts
@@ -4,10 +4,4 @@ export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
     await knex("users").del();
 
-    // Inserts seed entries
-    await knex("users").insert([
-        {username: "Toni", password: "tonisuperamazing" },
-        { username:"Dragon", password: "123d" },
-        { username:"Jojo", password: "jojojojo" }
-    ]);
 };
