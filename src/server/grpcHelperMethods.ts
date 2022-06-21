c4afb49f1b15f469d097558e5897aa09b908676b
Tonipenyallop <kimtesu521@gmail.com>
2022-06-21 19:34:59 +0900

    Refactor: remove comments

--- src/server/grpcHelperMethods.ts
+++ src/server/grpcHelperMethods.ts
@@ -4,12 +4,9 @@ import { Timestamp } from "../proto/google/protobuf/Timestamp";
 
 const database = require("../db/db");
 export async function checkValidSessionToken(sessionToken: string | undefined) {
-    // check authorized user
+
     const metadata = new grpc.Metadata();
-    // sessionToken = JSON.parse(sessionToken).sessionToken
-    // sessionToken = JSON.parse(req.body.sessionToken).sessionToken
   
-    // console.log(`sessionToken: ${sessionToken}`)
     if (!sessionToken) {
       // console.log("must print here")
       metadata.add("type", Exceptions.UNAUTHORIZED_USER_EXCEPTION.toString());
@@ -25,9 +22,8 @@ export async function checkValidSessionToken(sessionToken: string | undefined) {
       .from("sessions")
       .where("sessionToken", parsedSessionToken);
   
-      // console.log(`requestedUser: ${requestedUser}`)
   
-    // sessionToken is correct from database and requested token
+    // check token matches from database
     if (requestedUser.length === 0) {
       metadata.add("type", Exceptions.UNAUTHORIZED_USER_EXCEPTION.toString());
       return {
@@ -43,46 +39,7 @@ export async function checkValidSessionToken(sessionToken: string | undefined) {
       metadata,
     };
   }
-  // async function checkValidSessionToken(sessionToken: string | undefined) {
-  //   // check authorized user
-  //   const metadata = new grpc.Metadata();
-  //   // sessionToken = JSON.parse(sessionToken).sessionToken
-  //   // sessionToken = JSON.parse(req.body.sessionToken).sessionToken
   
-  //   console.log(`sessionToken: ${sessionToken}`)
-  //   if (!sessionToken) {
-  //     console.log("must print here")
-  //     metadata.add("type", Exceptions.UNAUTHORIZED_USER_EXCEPTION.toString());
-  //     return {
-  //       code: grpc.status.UNAUTHENTICATED,
-  //       message: "sessionToken is missing from request",
-  //       metadata,
-  //     };
-  //   }
-  //   const parsedSessionToken = JSON.parse(sessionToken).sessionToken;
-  //   const requestedUser = database
-  //     .select("*")
-  //     .from("sessions")
-  //     .where("sessionToken", parsedSessionToken);
-  
-  //     console.log(`requestedUser: ${requestedUser}`)
-  
-  //   // sessionToken is correct from database and requested token
-  //   if (requestedUser.length === 0) {
-  //     metadata.add("type", Exceptions.UNAUTHORIZED_USER_EXCEPTION.toString());
-  //     return {
-  //       code: grpc.status.UNAUTHENTICATED,
-  //       message: "sessionToken doesn't match from database",
-  //       metadata,
-  //     };
-  //   }
-  
-  //   return {
-  //     code: grpc.status.OK,
-  //     message: "sessionToken valid",
-  //     metadata,
-  //   };
-  // }
   
 export function convertToJSDate(jsDate: number): Date {
     // convert into milliseconds because Javascript expects milliseconds
@@ -94,27 +51,15 @@ export function fromDate(date: Date): Timestamp {
       seconds: date.getTime() / 1000,
     } as Timestamp;
   }
-  
-  // async function getUserId(sessionToken: string): Promise<number> {
-  //   const parsedSessionToken = JSON.parse(sessionToken as string).sessionToken;
-  //   const requestedUser = await database
-  //     .select("*")
-  //     .from("sessions")
-  //     .where("sessionToken", parsedSessionToken);
-  //   return requestedUser[0].userId;
-  // }
-  export async function getUserId(sessionToken: string): Promise<number>{
-    console.log(`getUserId`)
-    
-  
+
+export async function getUserId(sessionToken: string): Promise<number>{
     const parsedSessionToken = JSON.parse(sessionToken as string).sessionToken;
-    console.log(`parsedSessionToken: ${parsedSessionToken}`)
     const requestedUser = await database
       .select("*")
       .from("sessions")
       .where("sessionToken", parsedSessionToken);
   
-      if(requestedUser.length === 0) return Exceptions.UNAUTHORIZED_USER_EXCEPTION
+    if(requestedUser.length === 0) return Exceptions.UNAUTHORIZED_USER_EXCEPTION
     return requestedUser[0].userId;
   }
   
@@ -122,13 +67,10 @@ export async function getLatestReservation(sessionToken: string): Promise<any> {
     const userId = await getUserId(sessionToken);
     if(userId === Exceptions.UNAUTHORIZED_USER_EXCEPTION) {return Exceptions.UNAUTHORIZED_USER_EXCEPTION}
   
-  
     const notDeletedRequests = await database
       .select("*")
       .from("requests").where("userId", userId)
       .where("is_deleted", false);
-  
-      console.log(`notDeletedRequest: ${JSON.stringify(notDeletedRequests)}`)
   
     return notDeletedRequests[notDeletedRequests.length - 1];
   }
